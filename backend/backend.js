56625f5d44d0ed38fc09f291c411c612c79829af
derpmachine1 <ricksliu.mail@gmail.com>
2021-01-01 18:11:29 -0500

    Update to handle new normalized db

--- backend/backend.js
+++ backend/backend.js
@@ -18,7 +18,7 @@ let mysql_connection_pool = mysql.createPool({
 });
 
 // Returns first substring that is surrounded by 'start_char' and 'end_char'
-//'numeric' forces substring to start with a number
+// 'numeric' forces substring to start with a number
 // 'format' removes certain special characters
 function firstString(str, start_char, end_char, numeric=false, format=false) {  
   const len = str.length;
@@ -61,49 +61,66 @@ const server = http.createServer((req, res) => {
   // If frontend requested data from database
   if (request == 'get') {
     // Tries to get data from database
-    mysql_connection_pool.query(`SELECT * FROM mal_profile_data WHERE username = '${username}'`, (e, results, fields) => {
-      // If error or could not find matching username in database, sends response with empty user_id
-      if (e || results.length == 0) {
-        // Sets response's headers; designates response as JSON, allows requests from all domains
-        res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
-        // Sends JSON-encoded string
-        res.write(JSON.stringify({
-          user_id: ''
-        }));
-        res.end();
+    mysql_connection_pool.query(
+      `SELECT *
+      FROM (
+        SELECT *
+        FROM profiles
+        WHERE username = '${username}'
+      ) p JOIN (
+        SELECT *
+        FROM profile_stats
+        WHERE user_id = (
+          SELECT MIN(user_id)
+          FROM profiles
+          WHERE username = '${username}'
+        )
+      ) ps ON p.user_id = ps.user_id
+      ORDER BY id DESC`,
+      (e, results, fields) => {
+        // If error or could not find matching username in database, sends response with empty user_id
+        if (e || results.length == 0) {
+          // Sets response's headers; designates response as JSON, allows requests from all domains
+          res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+          // Sends JSON-encoded string
+          res.write(JSON.stringify({
+            user_id: ''
+          }));
+          res.end();
 
-        if (e) {
-          console.log(`Failed to get data for user '${username}' from database. (${e})`);
+          if (e) {
+            console.log(`Failed to get data for user '${username}' from databases. (${e})`);
+          } else {
+            console.log(`Failed to find data for user '${username}' in databases.`);
+          }
+
+        // Else, sends response with data
         } else {
-          console.log(`Failed to find user '${username}' in database.`);
+          // Sets response's headers; designates response as JSON, allows requests from all domains
+          res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+          // Sends JSON-encoded string
+          res.write(JSON.stringify({
+            user_id: results[0].user_id,
+            last_updated: results[0].date.toISOString().slice(0, 19).replace('T', ' ') + ' UTC',
+            username: username,
+            user_image: results[0].user_image,
+            mean_score: results[0].mean_score.toString().replace(/^0+/, ''),  // Removes leading zeroes
+            days_watched: results[0].days_watched.toString().replace(/^0+/, ''),  // Removes leading zeroes
+            episodes_watched: results[0].episodes_watched,
+            total_entries: results[0].total_entries,
+            completed: results[0].completed,
+            watching: results[0].watching,
+            on_hold: results[0].on_hold,
+            rewatched: results[0].rewatched,
+            dropped: results[0].dropped,
+            plan_to_watch: results[0].plan_to_watch
+          }));
+          res.end();
+
+          console.log(`Sent response with data for user '${username}' from databases.`);
         }
-
-      // Else, sends response with data
-      } else {
-        // Sets response's headers; designates response as JSON, allows requests from all domains
-        res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
-        // Sends JSON-encoded string
-        res.write(JSON.stringify({
-          username: username,
-          user_id: results[0].user_id,
-          last_updated: results[0].last_updated.toISOString().slice(0, 19).replace('T', ' ') + ' UTC',
-          user_image: results[0].user_image,
-          mean_score: results[0].mean_score.replace(/^0+/, ''),  // Removes leading zeroes
-          days_watched: results[0].days_watched.replace(/^0+/, ''),  // Removes leading zeroes
-          episodes_watched: results[0].episodes_watched,
-          total_entries: results[0].total_entries,
-          completed: results[0].completed,
-          watching: results[0].watching,
-          on_hold: results[0].on_hold,
-          rewatched: results[0].rewatched,
-          dropped: results[0].dropped,
-          plan_to_watch: results[0].plan_to_watch
-        }));
-        res.end();
-
-        console.log(`Sent response with data for user '${username}' from database.`);
       }
-    });
+    );
 
   // If frontend requested new data from MyAnimeList
   } else if (request == 'update') {
@@ -152,9 +169,9 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
         // Sends JSON-encoded string
         res.write(JSON.stringify({
-          username: username,
           user_id: info[0],
           last_updated: date + ' UTC',
+          username: username,
           user_image: `https://cdn.myanimelist.net/images/userimages/${info[0]}.jpg`,
           mean_score: info[1],
           days_watched: info[2],
@@ -171,22 +188,61 @@ const server = http.createServer((req, res) => {
 
         console.log(`Sent response with data for user '${username}' from MyAnimeList.`);
 
-        // Tries to update database with new data
-        // mysql.escape() adds single quotes automatically
+        // Tries to update 'profile' database
         mysql_connection_pool.query(
-          `REPLACE INTO mal_profile_data (
-            username, user_id, last_updated, user_image, mean_score, 
-            days_watched, episodes_watched, total_entries, completed, watching, 
-            on_hold, rewatched, dropped, plan_to_watch
+          `REPLACE INTO profiles (
+            user_id,
+            last_updated,
+            username,
+            user_image
           ) VALUES (
-            ${mysql.escape(username)}, ${info[0]}, ${mysql.escape(date)}, 'https://cdn.myanimelist.net/images/userimages/${info[0]}.jpg', ${info[1]}, 
-            ${info[2]}, ${info[3]}, ${info[4]}, ${info[5]}, ${info[6]}, 
-            ${info[7]}, ${info[8]}, ${info[9]}, ${info[10]}
-          )`, (e, results, fields) => {
+            ${info[0]},
+            ${mysql.escape(date)},
+            ${mysql.escape(username)},
+            'https://cdn.myanimelist.net/images/userimages/${info[0]}.jpg'
+          )`,
+          (e, results, fields) => {
             if (e) {
-              console.log(`Failed to insert data for user '${username}' into database. (${e})`);
+              console.log(`Failed to update profile for user '${username}' in database 'profiles'. (${e})`);
             } else {
-              console.log(`Inserted data for user '${username}' into database.`);
+              console.log(`Updated profile for user '${username}' in database 'profiles'.`);
+            }
+          }
+        );
+        // Tries to insert into 'profile_stats' database
+        mysql_connection_pool.query(
+          `INSERT INTO profile_stats (
+            date,
+            user_id,
+            mean_score, 
+            days_watched,
+            episodes_watched,
+            total_entries,
+            completed,
+            watching,
+            on_hold,
+            rewatched,
+            dropped,
+            plan_to_watch
+          ) VALUES (
+            ${mysql.escape(date)},
+            ${info[0]},
+            ${info[1]}, 
+            ${info[2]},
+            ${info[3]},
+            ${info[4]},
+            ${info[5]},
+            ${info[6]}, 
+            ${info[7]},
+            ${info[8]},
+            ${info[9]},
+            ${info[10]}
+          )`,
+          (e, results, fields) => {
+            if (e) {
+              console.log(`Failed to insert stats for user '${username}' into database 'profile_stats'. (${e})`);
+            } else {
+              console.log(`Inserted stats for user '${username}' into database 'profile_stats'.`);
             }
           }
         );
@@ -208,5 +264,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, host, () => {
-    console.log(`Backend Online: http://${host}:${port}/`);
+    console.log(`Online: http://${host}:${port}/`);
 });
