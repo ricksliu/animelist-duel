877d4b0fbc02135430883b04b99166e8d9112cc5
rick-liu-02 <ricksliu.mail@gmail.com>
2021-06-28 19:01:34 -0400

    setup

---
+++ src/main/java/com/github/ricksliu/animelist_duel/ProfileController.java
@@ -0,0 +1,16 @@
+package com.github.ricksliu.animelist_duel;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String profile(@RequestParam(name="username", required=true) String username, Model model) {
+        model.addAttribute("username", username);
+        return "profile";
+    }
+}
